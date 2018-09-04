4a902899d7276d9ec4aa2ce1a6e5f55d0de7f195
LordJax <40278484+LordJax@users.noreply.github.com>
2018-09-03 23:00:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.static('src'))
 
-app.get("/", (request, response) => {
+app.get("", (request, response) => {
   response.sendFile(__dirname + 'mainPage.html')
 })
 
